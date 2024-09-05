b9c17c21ea35aecf91154e52253c2006d2e4f6b9
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-05 12:47:21 +0800

    chore: Add initial project files and components

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
