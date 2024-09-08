c1bdd59e643cb0d0ff5951a8bda3813a918cbc8b
Kurt Carvey Cadenas <kurtcarvey.m.cadenas@gmail.com>
2024-09-08 20:22:26 +0800

    Refactor Star component to handle rating and hover events

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Star from "./Star";
 
 const containerSyle = {
@@ -8,7 +9,6 @@ const containerSyle = {
 
 const starContainerStyle = {
   display: "flex",
-  gap: "4px",
 };
 
 const textStyle = {
@@ -17,14 +17,27 @@ const textStyle = {
 };
 
 const StarRating = ({ maxRating = 5 }) => {
+  const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
+
+  const handleRating = (newRating) => {
+    setRating(newRating);
+  };
+
   return (
     <div style={containerSyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <Star key={i} />
+          <Star
+            key={i}
+            onRate={() => handleRating(i + 1)}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+          />
         ))}
       </div>
-      <p style={textStyle}>10</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 };
